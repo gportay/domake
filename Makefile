gportay/dosh

Makefile: add bump-PKGBUILD rule

Makefile
@@ -116,6 +116,11 @@ endif
 commit-check:
 	git rebase -i -x "$(MAKE) check && $(MAKE) tests"
 
+.PHONY: bump-PKGBUILD
+bump-PKGBUILD: aur
+	cp PKGBUILD.aur PKGBUILD
+	git commit PKGBUILD --patch --message "PKGBUILD: update release $$(bash dosh --version) checksum"
+
 .PHONY: clean
 clean:
 	rm -f dosh.1.gz

gportay/domake

Makefile
@@ -94,6 +94,11 @@ endif
 commit-check:
 	git rebase -i -x "$(MAKE) check && $(MAKE) tests"
 
+.PHONY: bump-PKGBUILD
+bump-PKGBUILD: aur
+	cp PKGBUILD.aur PKGBUILD
+	git commit PKGBUILD --patch --message "PKGBUILD: update release $$(bash domake --version) checksum"
+
 .PHONY: clean
 clean:
 	rm -f domake.1.gz