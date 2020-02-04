gportay/dosh

tests.sh: add --help and --version tests
To improve the test coverage.

tests.sh
@@ -115,6 +115,26 @@ else
 fi
 echo
 
+run "dosh: Test option --help"
+if dosh --help | \
+   grep '^Usage: '
+then
+	ok
+else
+	ko
+fi
+echo
+
+run "dosh: Test option --version"
+if dosh --version | \
+   grep -E '^([0-9a-zA-Z]+)(\.[0-9a-zA-Z]+)*$'
+then
+	ok
+else
+	ko
+fi
+echo
+
 run "dosh: Test with a binary argument"
 if ! dosh "$@" echo "one" "two" "three"
 then

gportay/domake

tests.sh
@@ -66,6 +66,26 @@ export -n DOSH_DOCKER_RMI_EXTRA_OPTS
 export -n DOSH_DOCKER_RUN_EXTRA_OPTS
 export -n DOSH_DOCKER_EXEC_EXTRA_OPTS
 
+run "domake: Test option --help"
+if domake --help | \
+   grep '^Usage: '
+then
+	ok
+else
+	ko
+fi
+echo
+
+run "domake: Test option --version"
+if domake --version | \
+   grep -E '^([0-9a-zA-Z]+)(\.[0-9a-zA-Z]+)*$'
+then
+	ok
+else
+	ko
+fi
+echo
+
 run "domake: Test default target with (Makefile from stdin)"
 if echo -e "all:\n\t@cat /etc/os*release" | \
    domake "$@" -f - | tee /dev/stderr | \